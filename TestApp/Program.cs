cdd1311836c8b9a9d797e043c70fb17fa5e1516d
Priya Palanki <priyapalanki@live.com>
2018-06-23 18:29:37 -0700

    FeatureBranch

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestApp
 {
@@ -11,7 +7,6 @@ namespace TestApp
         static void Main(string[] args)
         {
             Console.WriteLine("My First Test App");
-            Console.WriteLine(DateTime.Now.ToString());
             Console.ReadLine();
         }
     }
